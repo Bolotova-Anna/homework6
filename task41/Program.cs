7099e90f474e0d5fe4d7b54efdf0d7746bcaf6d4
Anna Bolotova <anechkamilaha69@gmail.com>
2023-03-01 20:43:00 +0300

    was done task 41

---
+++ task41/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте,
+//  сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+int[] FoundFillArray(string text, int number)
+{
+System.Console.WriteLine(text);
+int[] array = new int[number];
+array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+return  array;
+}
+void Print(int[] array)
+{
+    System.Console.WriteLine("["+string.Join(",", array)+"]");
+}
+void Count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i <array.Length; i++)
+    {
+        if (array[i]>0)
+        count++;
+    }
+    System.Console.WriteLine("пользоваель ввел "+ count +" положительных числа");
+}
+int[] myarray = FoundFillArray("введите числа для заполнения массива", 6);
+Print(myarray);
+Count(myarray);
